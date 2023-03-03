f87b9ec7950ab3c361c6c5a23772d0f774ae0e79
hugoIbzn <hugo.bazan@nomitek.com.mx>
2023-03-03 14:42:31 -0600

    Se eliminaron cosas innecesarias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AlertComponent } from './alert/alert.component';
-import { PopComponent } from './pop/pop.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AlertComponent,
-    PopComponent
   ],
   imports: [
     BrowserModule
