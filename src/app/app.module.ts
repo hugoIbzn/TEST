7598551cfb8b1d4a66af62a0158cf69db20ce6b7
hugoIbzn <hugo.bazan@nomitek.com.mx>
2023-03-06 09:55:32 -0600

    Se crea componente "pruebas" para dicho objetivo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AlertComponent } from './alert/alert.component';
+import { PruebasComponent } from './pruebas/pruebas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AlertComponent,
+    PruebasComponent,
   ],
   imports: [
     BrowserModule
